c5e45e71c51945ea247d12ece6f857891311a479
owenkruse <kruse.owen@vashonsd.org>
2023-02-05 21:30:25 -0800

    Swag

---
+++ main.py
@@ -0,0 +1,41 @@
+import yolov5
+
+"Grab the part of the screen"
+import pyscreenshot as ImageGrab
+import time
+
+# Grab the center of the screen 416x416
+
+im = ImageGrab.grab()
+
+# save image file
+im.save("box.png")
+
+model = yolov5.load('valorant-v12.pt')
+
+# set model parameters
+model.conf = 0.25  # NMS confidence threshold
+model.iou = 0.45  # NMS IoU threshold
+model.agnostic = False  # NMS class-agnostic
+model.multi_label = False  # NMS multiple labels per box
+model.max_det = 1000  # maximum number of detections per image
+
+img = 'box.png'  # or file, Path, PIL, OpenCV, numpy, list
+start = time.time()
+results = model(img, size=640)
+print(f'Done. ({time.time() - start:.3f}s)')
+
+results.show()
+
+
+# parse results
+predictions = results.pred[0]
+boxes = predictions[:, :4] # x1, y1, x2, y2
+scores = predictions[:, 4]
+categories = predictions[:, 5]
+
+# Find the center of the box
+center = (boxes[:, 0] + boxes[:, 2]) // 2, (boxes[:, 1] + boxes[:, 3]) // 2
+
+
+
---
+++ windows.py
@@ -0,0 +1,39 @@
+import yolov5
+from pyHM import mouse
+import dxcam
+camera = dxcam.create()
+
+import time
+
+# Grab the center of the screen 416x416
+
+im = camera.grab()
+
+model = yolov5.load('valorant-v12.pt')
+
+# set model parameters
+model.conf = 0.25  # NMS confidence threshold
+model.iou = 0.45  # NMS IoU threshold
+model.agnostic = False  # NMS class-agnostic
+model.multi_label = False  # NMS multiple labels per box
+model.max_det = 1000  # maximum number of detections per image
+
+img = im  # or file, Path, PIL, OpenCV, numpy, list
+start = time.time()
+results = model(img, size=640)
+print(f'Done. ({time.time() - start:.3f}s)')
+
+results.show()
+
+
+# parse results
+predictions = results.pred[0]
+boxes = predictions[:, :4] # x1, y1, x2, y2
+scores = predictions[:, 4]
+categories = predictions[:, 5]
+
+# Find the center of the box
+center = (boxes[:, 0] + boxes[:, 2]) // 2, (boxes[:, 1] + boxes[:, 3]) // 2
+
+
+
